0cc7e90f0b7d8b026e0b26924a0bec20af1b65b6
Raphael Rodrigues <raphael_rodrigues2012@hotmail.com>
2025-05-21 23:57:45 -0300

    Update usine_generation.py

--- src/usine_generation.py
+++ src/usine_generation.py
@@ -1,8 +1,9 @@
 import numpy as np
-from os.path import dirname, abspath
+from os.path import dirname, abspath, isfile
 from pathlib import Path
 from psutil import cpu_count
 from multiprocessing import Pool
+import pickle
 
 states:dict[str, str] = {
     "12": "AC", "27": "AL", "13": "AM", "16": "AP", "29": "BA", "23": "CE", "53": "DF",
@@ -10,7 +11,6 @@ states:dict[str, str] = {
     "25": "PB", "26": "PE", "22": "PI", "41": "PR", "33": "RJ", "24": "RN", "43": "RS",
     "11": "RO", "14": "RR", "42": "SC", "35": "SP", "28": "SE", "17": "TO"
 }
-
 
 def usine_plot(state:str, Z:np.ndarray) -> None:
     import matplotlib.pyplot as plt
@@ -25,31 +25,46 @@ def usine_plot(state:str, Z:np.ndarray) -> None:
 
     ax:Axes3D = plt.axes(projection='3d')
 
-    ax.plot_surface(X, Y, Z[:,:,1], cmap='viridis')
+    ax.plot_surface(X, Y, Z[:,:,1]/(10**6), cmap='viridis')
     ax.view_init(20, -50, 0)
     ax.set_xlabel('Day of the Data [Day]')
     ax.set_ylabel('Hour of the Day [Hour]')
     ax.set_zlabel('Power [MW]')
-    ax.set_title("Usines Generation Across (%02i/%i:%02i/%i)\n[%s]\n\nTotal produced: %.2f TWh"%(Z[0,0,0]%10000//100, Z[0,0,0]//10000, Z[-1,0,0]%10000//100, Z[-1,0,0]//10000, state, np.sum(Z[:,:,1])/(10**6)))
+    ax.set_title("Usines Generation Across (%02i/%i:%02i/%i)\n[%s]\n\nTotal produced: %.2f TWh"%(Z[0,0,0]%10000//100, Z[0,0,0]//10000, Z[-1,0,0]%10000//100, Z[-1,0,0]//10000, state, np.sum(Z[:,:,1])/(10**12)))
     plt.tight_layout()
     #plt.show()
     plt.savefig("%s\\outputs\\Usines MMD PV Generation\\usines-%s.png"%(Path(dirname(abspath(__file__))).parent, state), backend='Agg', dpi=200)
     plt.close()
 
+def usines_plot(state_Z_list:list[tuple[str, np.ndarray]]) -> None:
+    with Pool(cpu_count()) as p:
+        p.starmap(usine_plot, state_Z_list)
+
 def state_usines_pv_mmd_generation(state:str) -> tuple[str, np.ndarray]:
     with open('%s\\data\\usine_generation\\GERACAO_USINA-2_2000-2024_%s.csv'%(Path(dirname(abspath(__file__))).parent, state), 'r', 512*1024, encoding='utf-8') as f:
-            lines:list[str] = f.readlines()[1:]
+        lines:list[str] = f.readlines()[1:]
     
-    Z:np.ndarray = np.asarray([[[float(''.join(lines[j].split(';')[0][:10].split('-'))), float(lines[j].split(';')[1])] for j in range(i, i+24)] for i in range(0, len(lines), 24)])
+    Z:np.ndarray = np.zeros((len(lines), 2), np.int64)
+    i:int = 0
+    for line in lines:
+        spline:list[str] = line.split(';')
+        Z[i, 0] = spline[0][:10].replace('-', '')
+        Z[i, 1] = spline[1].replace('.', '')
+        i += 1
+    
+    return (state, Z.reshape((Z.shape[0]//24, 24, 2)))
 
-    return (state, Z)
-
-def usines_pv_mmd_generation(sts:list[str] = []) -> dict[str, np.ndarray]:
-
-    if not(sts):
-        sts = list(states.values())
+def usines_pv_mmd_generation() -> dict[str, np.ndarray]:
+    if (isfile('%s\\%s'%(Path(dirname(abspath(__file__))).parent, 'data\\pickles\\per_state_usines_pv_mmd_generation.pkl'))):
+        with open('%s\\%s'%(Path(dirname(abspath(__file__))).parent, 'data\\pickles\\per_state_usines_pv_mmd_generation.pkl'), 'rb', 8*1024*1024) as fin:
+            return pickle.load(fin)
 
     with Pool(cpu_count()) as p:
-        result:list[tuple] = p.map(state_usines_pv_mmd_generation, sts)
+        result:list[tuple] = p.map(state_usines_pv_mmd_generation, list(states.values()))
 
-    return {state:array for state, array in result}
+    per_state_usines_pv_mmd_generation:dict[str, np.ndarray] = {state:array for state, array in result}
+
+    with open('%s\\%s'%(Path(dirname(abspath(__file__))).parent, 'data\\pickles\\per_state_usines_pv_mmd_generation.pkl'), 'wb', 8*1024*1024) as fout:
+        pickle.dump(per_state_usines_pv_mmd_generation, fout)
+
+    return per_state_usines_pv_mmd_generation
